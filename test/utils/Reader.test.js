af97d863067ebeaff72fa913d18da5e57c6859d3
antoni-codes <emmanuel_general@hotmail.com>
2022-05-15 17:24:58 -0500

    Cambios

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,5 +1,5 @@
 const Reader = require('./../../lib/utils/Reader')
-
+// Reader
 describe('Pruebas de Reader Storyscroll', () => {
     test('1. Test de prubea', () => {
         const jsonFile = 'animales.json'
